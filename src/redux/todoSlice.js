5faf5910d474a94eb1d352e09753f0b90325e42d
annatill <anna.tataurova.95@gmail.com>
2023-06-15 19:54:07 +0200

    add todosSlise, store

---
+++ src/redux/todoSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const todoInitialState = {
+  todos: [],
+};
+
+export const todoSlice = createSlice({
+  name: 'todos',
+  initialState: todoInitialState,
+  reducers: {
+    addTodo: (state, action) => {
+      // state.todos.push(action.payload)
+      state.todos = [...state.todos, action.payload];
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter(todo => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todoSlice.actions;
+export const todosReducer = todoSlice.reducer;
+export const getTodos = state => state.todos.todos;
